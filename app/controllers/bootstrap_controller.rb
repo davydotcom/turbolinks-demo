809c2dff6588e7194520a6f48653dc211fe9feab
David Estes <destes@redwindsw.com>
2012-10-09 13:23:16 -0400

    first commit

---
+++ app/controllers/bootstrap_controller.rb
@@ -0,0 +1,2 @@
+class BootstrapController < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurbolinksDemo::Application.config.secret_token = 'f7589284eea7afa346b23421d6cfbf748cbfd9b34e2be8af61fd11ed8217d84c12292180d2624bd1372ddecda4d22f3203f777248857a68c4fb45622d0ed0ee2'
